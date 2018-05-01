6de4e21cb8d29e4d853bb8694739d7d0f7348c6c
PaulMaldonado <paul_pastore10@hotmail.com>
2018-05-01 11:39:38 -0500

    Primer ejercicio en javascript

---
+++ switch.js
@@ -0,0 +1,16 @@
+let dog = 'Zeus';
+
+switch (dog) {
+    case 'Zeus':
+    console.log("Es un perro muy bonito");
+    break;
+    case 'Cat':
+    console.log("No es un perro, es un gato");
+    break;
+    case 'Tigger':
+    console.log("No es ni gato, ni perro, es un tigre gruñon");
+    break;
+    default:
+    console.log("No es ninguna de las otras cosas es invalido");
+    break;
+}
