244342434cd55b62c0259821e004b815deec5a2c
Paúl <34814922+PaulMaldonado@users.noreply.github.com>
2018-05-01 16:39:27 -0500

    Update ternaryoperator.js
    Agregando dos ejemplos más de operadores ternarios.

--- ternaryoperator.js
+++ ternaryoperator.js
@@ -1,3 +1,13 @@
+// Devolvera no necesitas llaves.
 let isLocked = false;
 
 isLocked ? console.log('Necesitas una llave para abrir') : console.log('No necesitas llaves');
+
+// Devolvera Es correcto.
+let isCorrect = true;
+
+isCorrect ? console.log('Es correcto!') : console.log('No es correcto!');
+
+let myFavoriteAnimal = 'Dog';
+
+myFavoriteAnimal === 'Dog' ? console.log('Dog') : console.log('Incorrecto');
